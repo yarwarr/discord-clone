c1b7a05e65263e73dc6cf39cc5a1e251823f2701
Mubashar <maw8051@rit.edu>
2023-10-15 21:06:29 -0400

    Added all of the modals to create, edit, delete server and supported the backend for it too , added ability to see all of the channels

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,17 +1,26 @@
+import { ServerWithMembersWithProfiles } from '@/types';
+import { ChannelType, Server } from '@prisma/client';
 import { create } from 'zustand'
 
-export type ModalType = "createServer";
+export type ModalType = "createServer" | "invite" | "editServer" | "members" | "createChannel" | "leaveServer" | "deleteServer";
+
+interface ModalData {
+    server?: Server;
+    channelType?: ChannelType
+}
 
 interface ModalStore {
     type: ModalType | null;
+    data: ModalData;
     isOpen: boolean;
-    onOpen: (type: ModalType) => void;
+    onOpen: (type: ModalType, data?:ModalData) => void;
     onClose: () => void;
 } 
 
 export const useModal = create<ModalStore>((set) => ({
     type: null,
+    data: {},
     isOpen: false,
-    onOpen: (type) => set({isOpen: true, type}) ,
+    onOpen: (type, data= {}) => set({isOpen: true, type, data}) ,
     onClose: () => set({type: null, isOpen: false})
 }));
